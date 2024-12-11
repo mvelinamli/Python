8ed8e0cc659f5e8f2cabbefe7854a4d83996d007
Muhammed Veli NAMLI <162606231+mvelinamli@users.noreply.github.com>
2024-12-11 17:51:23 +0300

    Add files via upload

---
+++ tel_rehberi_with_dictionary.py
@@ -0,0 +1,11 @@
+#Dictionary kullanarak telefon rehberi oluştur.
+rehber={
+    "key":"value",
+    "isim":"numara",
+    "Veli":"1234567",
+    "Ali":"21345367589",
+}
+
+print(rehber)
+print(rehber["key"])
+print(rehber["Veli"])
